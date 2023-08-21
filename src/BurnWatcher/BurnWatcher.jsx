8fca1105eb3b89556281d7c39a7bb48024bb8d39
JeromeTGH <jerome.tomski@gmx.fr>
2023-08-21 17:39:19 +0200

    Correction bugs sur code 'BurnWatcher'

--- src/BurnWatcher/BurnWatcher.jsx
+++ src/BurnWatcher/BurnWatcher.jsx
@@ -6,12 +6,13 @@ const BurnWatcher = () => {
     // Constantes
     const minLuncToShow = 10000;    // Nombre de LUNC minimum pour une transaction donnée, pour que celle-ci soit "retenue" dans le tableau d'affichage final
     const minUstcToShow = 100;      // Nombre d'USTC minimum pour une transaction donnée, pour que celle-ci soit "retenue" dans le tableau d'affichage final
-    const nbLineToShow = 60;       // Nombre de lignes "filtrées" (que MsgSend, d'un certain montant), à afficher
+    const nbLineToShow = 30;       // Nombre de lignes "filtrées" (que MsgSend, d'un certain montant), à afficher
 
         
     // Variables React
     const [isLoading, setIsLoading] = useState(true);
     const [msgErreurGettingTransactions, setMsgErreurGettingTransactions] = useState();
+    const [tblTxsBurn, setTblTxsBurn] = useState([]);
 
 
     // Chargement au démarrage
@@ -22,11 +23,13 @@ const BurnWatcher = () => {
             if(res['erreur']) {
                 // Erreur
                 setMsgErreurGettingTransactions(res['erreur']);
+                setTblTxsBurn([]);
             }
             else {
                 // OK
                 setIsLoading(false);
                 setMsgErreurGettingTransactions('');
+                setTblTxsBurn(res);
             }
         })
     }, [])
@@ -43,7 +46,7 @@ const BurnWatcher = () => {
                     <div>Loading from blockchain (FCD), please wait ...</div>
                 :
                 <>
-                    <table>
+                    <table style={{ fontSize: "0.8rem"}}>
                         <thead>
                             <tr>
                                 <th>Date/Time</th>
@@ -54,7 +57,15 @@ const BurnWatcher = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            
+                            {tblTxsBurn.map((valeur, index) => {
+                                return <tr key={index}>
+                                    <td>{valeur[1].datetime}</td>
+                                    <td>{valeur[1].txHash}</td>
+                                    <td>{valeur[1].amount}</td>
+                                    <td>{valeur[1].account}</td>
+                                    <td>{valeur[1].memo}</td>
+                                </tr>
+                            })}
                         </tbody>
                     </table>
                 </>
--- src/BurnWatcher/getBurnTbl.js
+++ src/BurnWatcher/getBurnTbl.js
@@ -1,29 +1,26 @@
 import { tblCorrespondanceValeurs } from "../application/AppParams";
 import { FCDclient } from "../fcd/FCDclient";
 
-// Constantes globales
-const burnWalletAddress = 'terra1sk06e3dyexuq4shw77y3dsv480xv42mq73anxu';
-
-// Variable globale
-const tblBurns = [];
-
 export const getBurnTbl = async (minLuncToShow, minUstcToShow, nbLineToShow) => {
 
-    // Variables locales
-    let previous_nextid = 0;
-    let next_nextid = 0;
+    // Constantes
+    const burnWalletAddress = 'terra1sk06e3dyexuq4shw77y3dsv480xv42mq73anxu';
+    const tblBurns = [];
+    
+    // Variables
+    let nextid = 0;
     let nb_new_tx = 0;
+
 
     // Instanciation d'une classe de requetage FCD
     const fcd = FCDclient.getSingleton();
-
 
     // Boucle de chargement principal
     while(nb_new_tx < nbLineToShow) {
 
         // Préparation de la requête
         const params = new URLSearchParams();
-        params.append('offset', previous_nextid);
+        params.append('offset', nextid);
         params.append('account', burnWalletAddress);
 
         // Récupération des 100 dernières transactions, qui ont été enregistrées sur le burn wallet
@@ -38,12 +35,9 @@ export const getBurnTbl = async (minLuncToShow, minUstcToShow, nbLineToShow) => 
                 
                 // Sauvegarde du prochain ID à requéter, pour les 100 txs suivants
                 if(rawTxs.data.next)
-                    if(next_nextid === 0)
-                        next_nextid = rawTxs.data.next;
-                    else {
-                        previous_nextid = next_nextid;
-                        next_nextid = rawTxs.data.next;
-                    }
+                    nextid = rawTxs.data.next;
+                else
+                    break;
             
                 // Parcours de tous les txs
                 for(const tx of rawTxs.data.txs) {
@@ -54,86 +48,78 @@ export const getBurnTbl = async (minLuncToShow, minUstcToShow, nbLineToShow) => 
                     // On ne garde que les transactions réussies de type 'MsgSend direct', en excluant les msgs multiples et send "indirects"
                     if(txCode === 0 && tx.tx.value.msg.length === 1 && tx.tx.value.msg[0].type.includes('MsgSend')) {
 
-                        // Si l'ID de cette transaction ne figure pas dans notre tableau, alors on peut ajouter cette transaction à notre tableau
-                        if(!tblBurns[tx.id.toString()]) {
+                        // Calcul du nombre de LUNC ou autre envoyé au burn wallet (==> retourne une chaîne de string avec amount + denom)
+                        let txAmount = '';
+                        for(const coinAndDenom of tx.tx.value.msg[0].value.amount) {
+                            const coinAmount = parseInt(coinAndDenom.amount)/1000000;
+                            const coinDenom = tblCorrespondanceValeurs[coinAndDenom.denom] ? tblCorrespondanceValeurs[coinAndDenom.denom] : coinAndDenom.denom;
 
-                            // Calcul du nombre de LUNC ou autre envoyé au burn wallet (==> retourne une chaîne de string avec amount + denom)
-                            let txAmount = '';
-                            for(const coinAndDenom of tx.tx.value.msg[0].value.amount) {
-                                const coinAmount = parseInt(coinAndDenom.amount)/1000000;
-                                const coinDenom = tblCorrespondanceValeurs[coinAndDenom.denom] ? tblCorrespondanceValeurs[coinAndDenom.denom] : coinAndDenom.denom;
-
-                                // Scanne les potentiels LUNC
-                                if(coinDenom === 'LUNC' && coinAmount >= minLuncToShow) {
-                                    if(txAmount === '')
-                                        txAmount = coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
-                                    else
-                                        txAmount += ', ' + coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
-                                }
-
-                                // Scanne les potentiels USTC
-                                if(coinDenom === 'USTC' && coinAmount >= minUstcToShow) {
-                                    if(txAmount === '')
-                                        txAmount = coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
-                                    else
-                                        txAmount += ', ' + coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
-                                }
+                            // Scanne les potentiels LUNC
+                            if(coinDenom === 'LUNC' && coinAmount >= minLuncToShow) {
+                                if(txAmount === '')
+                                    txAmount = coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
+                                else
+                                    txAmount += ', ' + coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
                             }
-                                
-                            // On garde cette transaction, si elle est "bonne", après filtrage
-                            if(txAmount !== '') {
-                                // Incrémentation du nombre de MsgSend lus (pour être ensuite comparé au "nbLineToShow")
-                                nb_new_tx = nb_new_tx + 1;
 
-                                // Structure d'enregistrement :
-                                //      tblBurn[id] = {
-                                //          datetime,
-                                //          txHash,
-                                //          amount,
-                                //          account,
-                                //          memo
-                                //      }
-                                tblBurns[tx.id.toString()] = {
-                                    'datetime': tx.timestamp,
-                                    'txHash': tx.txhash,
-                                    'amount': txAmount,
-                                    'account': tx.tx.value.msg[0].value.from_address,
-                                    'memo': tx.tx.value.memo
-                                }
-    
-                                // Et si on a lu assez de "lignes", alors on peut alors arrêter la lecture de transactions (en quittant la boucle "for")
-                                if(nb_new_tx >= nbLineToShow)
-                                    break;
-
+                            // Scanne les potentiels USTC
+                            if(coinDenom === 'USTC' && coinAmount >= minUstcToShow) {
+                                if(txAmount === '')
+                                    txAmount = coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
+                                else
+                                    txAmount += ', ' + coinAmount.toFixed(6) + '\u00a0' + coinDenom;    // Nota : \u00a0 est un 'espace insécable', en javascript
                             }
-                        } else {
-                            // Si l'ID de cette transaction existe déjà dans le tableau, on n'a plus qu'à la compter comme déjà chargée
-                            // (d'où l'incrémentation du nombre de MsgSend lus, pour être ensuite comparé au "nbLineToShow")
+                        }
+                            
+                        // On garde cette transaction, si elle est "bonne", après filtrage
+                        if(txAmount !== '') {
+                            // Incrémentation du nombre de MsgSend lus (pour être ensuite comparé au "nbLineToShow")
                             nb_new_tx = nb_new_tx + 1;
+
+                            // Structure d'enregistrement :
+                            //      tblBurn[id] = {
+                            //          datetime,
+                            //          txHash,
+                            //          amount,
+                            //          account,
+                            //          memo
+                            //      }
+                            tblBurns[tx.id.toString()] = {
+                                'datetime': tx.timestamp,
+                                'txHash': tx.txhash,
+                                'amount': txAmount,
+                                'account': tx.tx.value.msg[0].value.from_address,
+                                'memo': tx.tx.value.memo
+                            }
 
                             // Et si on a lu assez de "lignes", alors on peut alors arrêter la lecture de transactions (en quittant la boucle "for")
                             if(nb_new_tx >= nbLineToShow)
                                 break;
+
                         }
                     }
                 }
                     
             } else
-                return { "erreur": "Failed to fetch [first rawTxs.data] ..." }
+                return { "erreur": "Failed to fetch [rawTxs.data] ..." }
         } else
             return { "erreur": "Failed to fetch [burn wallet txs] ..." }
 
 
-        // Rebouclage while
-        // console.log("Avancement : " + nb_new_tx + "/" + nbLineToShow);
+        // Rebouclage while, tant qu'on a pas le nombre suffisant de transactions à afficher
 
     }
 
 
-    console.log("tblBurns", tblBurns);
+    // Manips, avant restitution :
+    // - "entries" permet la conversions de object à array
+    // - "sort" permet de trier par clef (qui sont les 'id' de transaction, ici)
+    // - "slice" permet de ne garder que les X premiers résultats (des fois qu'il y en ait plus, du fait qu'on mémoire les 'anciens')
+    const arrBurns = Object.entries(tblBurns).sort((a, b) => {return b[0] - a[0]}).slice(0, nbLineToShow);
+    // console.log("arrBurns", arrBurns);
 
-    // Signale que le chargement s'est passé sans pb
-    return {}
+    // Renvoie le tableau final, pour affichage
+    return arrBurns;
 }
 
 const handleError = (err) => {
